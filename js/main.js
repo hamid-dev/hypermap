263532168fff506469439799c0cb521a2b2358c7
hamid heydari <heydari.h62@gmail.com>
2017-10-02 13:36:56 +0330

    refactor side nav slide show

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
 let menuButton = document.getElementById('nav-button');
+let clostButton = document.getElementById('close-btn');
 
-menuButton.addEventListener('click', (evt) => {
-    evt = evt || window.event;
+clostButton.addEventListener('click', (event) => {
+    event = event || window.event;
+    showHideSideNav(false);
+});
 
+menuButton.addEventListener('click', (event) => {
+    event = event || window.event;
+    showHideSideNav(true);
+});
+
+function showHideSideNav(show){
     let sideNav = document.getElementsByClassName('side-nav')[0];
     let sideHeader = document.getElementsByClassName('site-header')[0];
 
-    if (!sideNav.style.width || sideNav.style.width === '0px') {
+    if (show) {
         sideNav.style.width = "200px";
-        sideHeader.style.marginLeft="200px";      
+        sideHeader.style.marginLeft="200px";  
+        menuButton.style.display = 'none';
     }
     else {
         sideHeader.style.marginLeft="0";
         sideNav.style.width = "0";  
+        menuButton.style.display = 'inline';
     }
-
-});
+}
